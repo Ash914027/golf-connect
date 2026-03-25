cece746cd2fc7f18f8e836e2a2082894acdde3e8
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2026-03-26 00:56:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './context/AuthContext';
+
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
